3f6d232c61e0fadfd9ac8f3764f199ebb84733bc
Leandro <leandro.db1start@gmail.com>
2019-11-13 16:51:26 -0300

    13/11

--- src/com/db1/db1start/AppTest.java
+++ src/com/db1/db1start/AppTest.java
@@ -2,6 +2,7 @@ package com.db1.db1start;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
 
 public class AppTest {
 
@@ -31,6 +32,14 @@ public class AppTest {
     }
 
     @Test
+    public void deveSubtrairEretornar9(){
+        Application application = new Application();
+        Integer expected = 9;
+        Integer response = application.subtracao(18,9);
+        Assert.assertEquals(expected, response);
+    }
+
+    @Test
     public void deveMultiplicarERetornar15(){
         Application application = new Application();
         Integer expected = 15;
@@ -55,10 +64,116 @@ public class AppTest {
     }
 
     @Test
+    public void maior(){
+        Application application = new Application();
+        Integer expected = 10;
+        Integer response = application.maior(10,8);
+        Assert.assertEquals(expected, response);
+    }
+
+    @Test
     public void quantosImpares(){
         Application application = new Application();
         Integer expected = 5;
         Integer response = application.quantosImpares(10);
         Assert.assertEquals(expected,response);
+    }
+
+    @Test
+    public void maiusculo(){
+        Application application = new Application();
+        String expected = "LEANDRO";
+        String response = application.maiusculo("leandro");
+        Assert.assertEquals(expected, response);
+    }
+
+    @Test
+    public void minusculo(){
+        Application application = new Application();
+        String expected = "leandro";
+        String response = application.minusculo("LEANDRO");
+        Assert.assertEquals(expected, response);
+    }
+
+    @Test
+    public void contaLetrasDB1start(){
+        Application application = new Application();
+        Integer expected = 8;
+        Integer response = application.contaLetrasDB1start("DB1Start");
+        Assert.assertEquals(expected, response);
+    }
+
+    @Test
+    public void contaLetrasSemEspacos(){
+        Application application = new Application();
+        Integer expected = 8;
+        Integer response = application.contaLetrasSemEspacos(" DB1Start ");
+        Assert.assertEquals(expected, response);
+    }
+
+    @Test
+
+    public void apenasPrimeirasLetras(){
+        Application application = new Application();
+        String expected = "Lean";
+        String response = application.apenasPrimeirasLetras("Leandro Mikio Takahashi");
+        Assert.assertEquals(expected, response);
+    }
+
+    @Test
+
+    public void aPartirDaTerceiraLetra(){
+        Application application = new Application();
+        String expected = ("ndro Mikio Takahashi");
+        String response = application.aPartirDaTerceiraLetra("Leandro Mikio Takahashi");
+        Assert.assertEquals(expected, response);
+    }
+
+    @Test
+    public void apenasUltimasLetras(){
+        Application application = new Application();
+        String expected = ("ashi");
+        String response = application.apenasUltimasLetras("Leandro Mikio Takahashi");
+        Assert.assertEquals(expected, response);
+    }
+
+    /*@Test  //Este teste não ficou dinâmico
+    public void trocaNomePorAluno(){
+        Application application = new Application();
+        String expected = ("Aluno Mikio Takahashi");
+        String response = application.trocaNomePorAluno("Leandro Mikio Takahashi");
+        Assert.assertEquals(expected, response);
+    }*/
+
+    @Test
+    public void trocaNomePorAluno(){
+        Application application = new Application();
+        String expected = ("Aluno Mikio Takahashi");
+        String response = application.trocaNomePorAluno("Leandro Mikio Takahashi");
+        Assert.assertEquals(expected, response);
+    }
+
+    @Test
+    public void separaPalavras(){
+        Application application = new Application();
+        String[] expected = new String[]{"banana", "maçã", "melancia"};
+        String[] response = application.separaPalavras("banana, maçã, melancia");
+        Assert.assertEquals(expected, response);
+    }
+
+    /*@Test
+    public void contarVogais(){
+        Application application = new Application();
+        Integer expected = 10;
+        String response = application.contaVogais("Leandro Mikio Takahashi");
+        Assert.assertEquals(expected, response);
+    }*/
+
+    @Test
+    public void deveContarQuantidadeDeVogais(){
+        Application application = new Application();
+        Integer expected = 3;
+        Integer response = application.quantidadeVogais("LEANDRO");
+        Assert.assertEquals(expected, response);
     }
 }
--- src/com/db1/db1start/Application.java
+++ src/com/db1/db1start/Application.java
@@ -6,6 +6,10 @@ public class Application {
         return numero1 + numero2;
     }
 
+    public Integer subtracao (Integer numero1, Integer numero2){
+        return numero1 - numero2;
+    }
+
     public Integer multiplicacao (Integer numero1, Integer numero2){
         return numero1 * numero2;
     }
@@ -16,6 +20,13 @@ public class Application {
 
     public Integer par (Integer numero1){
         return numero1 % 2;
+    }
+
+    public Integer maior(Integer numero1, Integer numero2){
+        if (numero1 > numero2){
+            return numero1;
+        }
+        return numero2;
     }
 
     public Integer quantosImpares(Integer numero1){
@@ -31,5 +42,91 @@ public class Application {
             i++;
         }
         return contador;
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------
+
+    public String maiusculo(String texto){
+        String textoMaiusculo = texto.toUpperCase();
+        return textoMaiusculo;
+
+    }
+
+    public String minusculo(String texto){
+        String textoMinusculo = texto.toLowerCase();
+        return textoMinusculo;
+
+    }
+
+    public Integer contaLetrasDB1start(String texto){
+        Integer quantidadeDeLetrasDB1start = texto.length();
+        return quantidadeDeLetrasDB1start;
+    }
+    private String removeNumerosDaString(String texto){
+        String removeNumerosDaString;
+        return texto.replaceAll("[0-9]", "");
+    }
+
+
+    public Integer contaLetrasSemEspacos(String texto){
+        String textoSemEspacos = texto.trim();
+        Integer quantasLetras = textoSemEspacos.length();
+        return quantasLetras;
+    }
+
+    public String apenasPrimeirasLetras(String texto){
+        String primeirasLetras = texto.substring(0,4);
+        return primeirasLetras;
+    }
+
+    public String aPartirDaTerceiraLetra(String texto){
+        String restoDaPalavra = texto.substring(3);
+        return restoDaPalavra;
+    }
+
+    public String apenasUltimasLetras(String texto){
+        String textoSemEspacos = texto.trim();
+        Integer contaLetras = textoSemEspacos.length();
+        String ultimasLetras = textoSemEspacos.substring(contaLetras-4);
+        return ultimasLetras;
+    }
+
+    public String trocaNomePorAluno(String texto){
+        Integer indiceEspaco = texto.indexOf(" ");
+        String nomeTratado = texto.substring(indiceEspaco);
+        String aluno = "Aluno";
+        return aluno.concat(nomeTratado);
+    }
+
+    /*public String trocaNomePorAluno(String texto){
+        String trocaNome = texto.replaceAll("Leandro", "Aluno");
+        return trocaNome;
+    }*/
+
+    public String[] separaPalavras(String texto){
+        return texto.split(", ");
+    }
+
+    /*public Integer contaVogais(String texto){
+        Integer contador = 0;
+        texto.toLowerCase();
+
+        for (Integer i = 0; i < texto.length(); i++){
+            Integer letra = texto.charAt(i);
+            if (letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u"){
+                contador++;
+            }
+        }
+
+        return contador;
+    }*/
+
+    public Integer quantidadeVogais(String texto){
+        return texto
+                .trim()
+                .replace(" ", "")
+                .toUpperCase()
+                .replaceAll("[^AEIOU]", "")
+                .length();
     }
 }
